97c42191b1ecfda69800396efd5b95ec914803d1
SudiptoChoudhury <mail@sudipto.net>
2018-12-13 10:28:29 +0530

    Disabled log by default

--- src/ClubSpeed/Enterprise/EventsKarting/Api.php
+++ src/ClubSpeed/Enterprise/EventsKarting/Api.php
@@ -25,6 +25,7 @@ class Api extends ApiForge
         'client' => [
             'base_uri' => 'http://f1phx.clubspeedtiming.com/api/index.php/',
         ],
+        'log' => false,
         'settings' => [
             'responseHandler' => null,
             'requestHandler' => null,
